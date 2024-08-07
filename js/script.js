e99268a57fc0b21a8e93aa55e88d0225aa368a81
Vinícius de Menezes Lima <42392897+AlphaJooj@users.noreply.github.com>
2024-08-07 09:31:59 -0300

    Reverting to Original committed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-// Mude a cor da tela para azul e depois para vermelho a cada 2s.
-function mudarClasse(){
-    console.log('sauble');
-}
-// Crie um cronometro utilizando o setInterval. Deve ser possível
-// iniciar, pausar e resetar (duplo clique no pausar).
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordeon from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from './modules/modal.js';
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+initScrollSuave();
+initAnimacaoScroll();
+initAccordeon();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
