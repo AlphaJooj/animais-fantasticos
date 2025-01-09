ca8e55bc8fd51ab2960d2824b9327e2267e831ed
Vinícius de Menezes Lima <vinixd476@gmail.com>
2025-01-09 09:30:14 -0300

    Adicionando slide de fotos

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
-import ScrollAnima from "./modules/scroll-anima.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
 import Funcionamento from "./modules/funcionamento.js";
+import fetchAnimais from "./modules/fetch-animais.js";
+import fetchBitcoin from './modules/fetch-bitcoin.js';
+import ScrollAnima from "./modules/scroll-anima.js";
+import SlideNav from './modules/slide.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -44,5 +45,10 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-initFetchAnimais();
-initFetchBitcoin();
+fetchAnimais('.animaisapi.json', '.numeros-grid');
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
